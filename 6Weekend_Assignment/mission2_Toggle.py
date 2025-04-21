79b3b8f4312fed9513e528937f4c969f9f1fb321
SelectMe02 <149069604+SelectMe02@users.noreply.github.com>
2025-04-22 02:30:47 +0900

    Update mission2_Toggle.py

--- 6Weekend_Assignment/mission2_Toggle.py
+++ 6Weekend_Assignment/mission2_Toggle.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python3
+
+from gpiozero import LED, Button
+from time import sleep
+
+button = Button(25)
+leds = [LED(pin) for pin in [7, 8, 16, 20]]
+
+state = False
+prev = 1
+
+while True:
+    curr = button.is_pressed
+    if curr and not prev:
+        state = not state
+        for led in leds:
+            if state:
+                led.on()
+            else:
+                led.off()
+    prev = curr
+    sleep(0.05)
 
