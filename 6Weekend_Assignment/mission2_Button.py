baa695300bfbc791b18f85983f4b9f84a3d4c3ed
SelectMe02 <149069604+SelectMe02@users.noreply.github.com>
2025-04-22 02:27:43 +0900

    Update mission2_Button.py

--- 6Weekend_Assignment/mission2_Button.py
+++ 6Weekend_Assignment/mission2_Button.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
+
+from gpiozero import LED, Button
+from time import sleep
+
+button = Button(25)
+leds = [LED(pin) for pin in [7, 8, 16, 20]]
+
+while True:
+    if button.is_pressed:
+        for led in leds:
+            led.on()
+    else:
+        for led in leds:
+            led.off()
+    sleep(0.05)
+
 
